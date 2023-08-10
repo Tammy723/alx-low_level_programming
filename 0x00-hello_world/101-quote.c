69c8389af4de7c9912575d2ff964ac9efe257bcb
Tammy <satti.f@gmail.com>
2023-08-10 14:40:40 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,14 @@
 /**
  * main - Entry point
  *
- * Return: Always 1 (Success)
+ * Discription: print a quote using the write function
+ *
+ * Return: Always 1 (Not Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
 	return (1);
 }
