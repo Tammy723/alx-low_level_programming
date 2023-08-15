0562810ee009194f6726d1570f2947c82fbde904
Tammy <satti.f@gmail.com>
2023-08-15 10:14:13 -0700

    Task 0 A program to print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints _putchar, followed by a new line
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
