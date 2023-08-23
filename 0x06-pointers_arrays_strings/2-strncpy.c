a4e55801a68732711aaac30eab8d49f15bd900ff
Tammy <satti.f@gmail.com>
2023-08-23 09:39:59 -0700

    Task 2 a function that copys a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_strncpy - a function that copies a string
+ * @dest: pointer to the destination input
+ * @src: pointer to the source input
+ * @n: bytes of source
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n &&  src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
