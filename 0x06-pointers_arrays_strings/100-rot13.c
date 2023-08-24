fb7c8c9dc895864551e6be2bcfc2f383781f744e
Tammy <satti.f@gmail.com>
2023-08-24 12:11:56 -0700

    Task 8 a function that encodes a string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - encoder rot13
+ * @s: pointer to string params
+ *
+ * Return: *s
+ */
+
+char *rot13(char *s)
+{
+	int i;
+	int t;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (t = 0; t < 52; t++)
+		{
+			if (s[i] == data1[t])
+			{
+				s[i] = datarot[t];
+				break;
+			}
+		}
+	}
+	return (s);
+}
